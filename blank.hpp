8ed29ff33ee2e443a4226251df23e5594a3cd502
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-10-28 20:19:31 +0200

    [Enhancement] Add Frames around GPL-2.0 License Text (#13)
    * Update blank.l
    
    * Update blank.hpp
    
    * Update blank.h
    
    * Update blank.cpp
    
    * Update blank.c
    
    * Create feature:license-frame.rst
    
    * Update blank.hpp
    
    * Update feature:license-frame.rst
    
    * Update blank.hs
    
    * Update feature:license-frame.rst

--- blank.hpp
+++ blank.hpp
@@ -1,27 +1,29 @@
-/*
- * Copyright (C) 2022 Kevin Matthes
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+/*********************** GNU General Public License 2.0 ***********************\
+|                                                                              |
+|  Copyright (C) 2022 Kevin Matthes                                            |
+|                                                                              |
+|  This program is free software; you can redistribute it and/or modify        |
+|  it under the terms of the GNU General Public License as published by        |
+|  the Free Software Foundation; either version 2 of the License, or           |
+|  (at your option) any later version.                                         |
+|                                                                              |
+|  This program is distributed in the hope that it will be useful,             |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               |
+|  GNU General Public License for more details.                                |
+|                                                                              |
+|  You should have received a copy of the GNU General Public License along     |
+|  with this program; if not, write to the Free Software Foundation, Inc.,     |
+|  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                 |
+|                                                                              |
+\******************************************************************************/
 
 /**
  * \author      Kevin Matthes
  * \brief       .
  * \copyright   GPL-2.0
  * \date        2022
- * \file        .h
+ * \file        .hpp
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  */
@@ -37,8 +39,8 @@
  */
 
 #pragma once
-#ifndef ___H__
-#define ___H__
+#ifndef ___HPP__
+#define ___HPP__
 
 
 
@@ -99,6 +101,6 @@
 #endif  // ! ___INTERNAL__
 
 // Leaving the header.
-#endif  // ! ___H__
+#endif  // ! ___HPP__
 
 /******************************************************************************/
---
+++ changelog.d/feature:license-frame.rst
@@ -0,0 +1,14 @@
+Added
+.....
+
+- add frames around GPL-2.0 license text in all templates where applicable
+
+Changed
+.......
+
+- adjust spacing in Haskell template license frame
+
+Fixed
+.....
+
+- file extension ``*.hpp`` in C++ header template description and macros
