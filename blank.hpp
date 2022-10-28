f062e0580aba403717a1e7ed787b265de023daa0
Kevin Matthes <92332892+kevinmatthes@users.noreply.github.com>
2022-10-28 19:15:24 +0200

    Update blank.hpp

--- blank.hpp
+++ blank.hpp
@@ -19,8 +19,7 @@
 /**
  * \author      Kevin Matthes
  * \brief       .
- * \copyright   (C) 2022 Kevin Matthes.
- *              This file is licensed GPL 2 as of June 1991.
+ * \copyright   GPL-2.0
  * \date        2022
  * \file        .h
  * \note        See `LICENSE' for full license.
